53519a2c6aea34d6c25fe5848b8650877b79efec
Daksh Dave <146529998+DakshDAVE77@users.noreply.github.com>
2024-06-06 10:42:26 +0000

    Changes

--- Basics/comparision.js
+++ Basics/comparision.js
@@ -1,4 +1,12 @@
 let score='hitesh'
 let beANumber=Number(score)
 
-console.log(typeof beANumber);
+console.log(typeof beANumber);//Type will be a number 
+
+console.log(beANumber);//But the value will return not a number
+
+let login='';
+let boollogin= Boolean(login);
+console.log(boollogin);
+// any string the answer of the boolean will be true;
+//for '' the answer would be false
