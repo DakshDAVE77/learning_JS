0f41dbdd532e45200d7e842423ca2e7ed452e46f
Daksh Dave <146529998+DakshDAVE77@users.noreply.github.com>
2024-06-06 09:49:54 +0000

    Checking variable

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,14 @@
-console.log("Daksh");
+const accountId=123000
+let accountEmail='daksh@gmail.com'
+accountCity="Ahmedabad"
+
+//accountId=49000
+
+console.log(accountId);
+
+accountEmail='milesh@gmail.com'
+
+console.table([accountId,accountEmail,accountCity]);
+
+//var not used because of issues in block scope and functional scope
+
