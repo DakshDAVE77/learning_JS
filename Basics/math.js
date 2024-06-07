89c92449c031b5c9258f3763e5692f7088c00224
Daksh Dave <146529998+DakshDAVE77@users.noreply.github.com>
2024-06-07 16:18:37 +0000

    The Learning Curve

--- Basics/math.js
+++ Basics/math.js
@@ -7,3 +7,6 @@ const max=20
 
 console.log(Math.floor(Math.random() * (max-min+1)) + min);
 // Math floor is used for the lower limit of any number Eg-5.9 to 5
+
+let mydate= new Date
+console.log(mydate.toString())
