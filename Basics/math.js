8ea55262fa5df53473c2743afbead49a230b9649
Daksh Dave <146529998+DakshDAVE77@users.noreply.github.com>
2024-06-07 11:21:13 +0000

    Exploring More on JS

---
+++ Basics/math.js
@@ -0,0 +1,9 @@
+console.log(Math.floor);
+
+//For randomly generation random values for questions
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+// Math floor is used for the lower limit of any number Eg-5.9 to 5
---
+++ Basics/strings.js
@@ -0,0 +1,7 @@
+let name='Daksh'
+
+let number=50
+
+console.log(`My name is ${name}`);
+const sent= String("Tesla")
+console.log(name.fontcolor("blue"));
